e1f4416e3debfd3dd4825a93088590e2ac4496dd
mohamedaminemekki <MohamedAmine.Mekki@esprit.tn>
2025-05-15 12:09:07 +0200

    updated design

--- src/main/java/HelloApplication.java
+++ src/main/java/HelloApplication.java
@@ -2,6 +2,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.Screen;
+import javafx.geometry.Rectangle2D;
 
 import java.io.IOException;
 
@@ -9,11 +11,27 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/amine/userModule/login-view.fxml"));
+        
+        // Get screen dimensions
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
 
-        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
-        scene.getStylesheets().add(getClass().getResource("/amine/userModule/style.css").toExternalForm());
-        stage.setTitle("Hello!");
+        // Load the root node
+        Scene scene = new Scene(fxmlLoader.load());
+        
+        // Set the stage size to match screen size
+        stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(bounds.getWidth());
+        stage.setHeight(bounds.getHeight());
+        
+        // Set application title
+        stage.setTitle("MDINTECH");
         stage.setScene(scene);
+        
+        // Make it maximized by default
+        stage.setMaximized(true);
+        
         stage.show();
     }
 
